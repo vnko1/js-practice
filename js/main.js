a718c206c1c085ff95cfb7cde0c66f4b63271efb
Andrey Valenko <andreyvalenko@gmail.com>
2022-12-02 12:47:35 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// for (let i = 1; i <= 60; i++) {
+//   let quarterValue = Math.ceil(i / 15);
+//   console.log(`${i} - це ${quarterValue} чверть.`);
+// }
+
+// const timeValue = Math.round(Math.random() * (60 - 1) + 1);
+// const timeValue = 61;
+
+// if (timeValue <= 15) {
+//   console.log(`1 чверть години`);
+// } else if (timeValue > 15 && timeValue <= 30) {
+//   console.log(`2 чверть години`);
+// } else if (timeValue > 30 && timeValue <= 45) {
+//   console.log(`3 чверть години`);
+// } else if (timeValue > 45 && timeValue < 60) {
+//   console.log(`4 чверть години`);
+// } else {
+//   const hoursValue = Math.floor(timeValue / 60);
+//   const quarterValue = Math.ceil((timeValue % 60) / 15);
+//   if (timeValue % 60 === 0) {
+//     console.log(`${hoursValue} година`);
+//   } else {
+//     console.log(`${quarterValue} чверть ${hoursValue} години `);
+//   }
+// }
+
+// const timeValue = 360;
+// if (timeValue <= 60) {
+//   console.log(`${Math.ceil(timeValue / 15)} чверть години`);
+// } else {
+//   const hoursValue = Math.floor(timeValue / 60);
+//   const quarterValue = Math.ceil((timeValue % 60) / 15);
+//   if (timeValue % 60 === 0) {
+//     console.log(`${hoursValue} година`);
+//   } else {
+//     console.log(`${quarterValue} чверть ${hoursValue} години `);
+//   }
+// }
