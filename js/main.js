5d93dff93a21d6c05f2fcbd0843cdd87af917baa
Andrey Valenko <andreyvalenko@gmail.com>
2022-12-23 17:21:49 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,3 +61,29 @@
 //   }
 //   return indexArr;
 // }
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+  const arr = [];
+  let catYears = 0;
+  let dogYears = 0;
+  for (let i = 1; i <= humanYears; i++) {
+    if (i === 1) {
+      catYears += 15;
+      dogYears += 15;
+    }
+    if (i === 2) {
+      catYears += 9;
+      dogYears += 9;
+    }
+    if (i > 2) {
+      catYears += 4;
+      dogYears += 5;
+    }
+  }
+
+  arr.push(humanYears, catYears, dogYears);
+
+  return arr;
+};
+
+console.log(humanYearsCatYearsDogYears(10));
