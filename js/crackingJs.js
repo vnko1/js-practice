7ec02cb936e1538c93f4f602b545b0bcd67fabd7
Andrii Valenko <andreyvalenko@gmail.com>
2023-03-17 17:08:04 +0200

    s

---
+++ js/crackingJs.js
@@ -0,0 +1,25 @@
+// class User {
+//   constructor(par) {
+//     this.name = par.name;
+//     this.age = par.age;
+//   }
+
+//   getName() {
+//     console.log('object');
+//     return this.name;
+//   }
+// }
+
+// class Car extends User {
+//   constructor({ type, ...rest }) {
+//     super(rest);
+//     this.type = type;
+//   }
+//   getCar() {
+//     return super.getName();
+//   }
+// }
+// const car = new Car({ name: 'AA', age: 20, type: 'BMW' });
+
+// console.log(car.getCar());
+// --------------------------------------------------
