50efcdd2eb13866d0f791dd8915f3e82465119da
Andrii Valenko <andreyvalenko@gmail.com>
2023-03-25 09:00:06 +0200

    s

---
+++ js/hw.js
@@ -0,0 +1,25 @@
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// async function logNumber() {
+//   setInterval( () => {
+//     getData();
+//   }, 3000);
+// }
+
+// function foo() {
+//   const data = arr.splice(0, 3);
+//   return new Promise(onres => {
+//     setTimeout(() => {
+//       onres(data);
+//     }, 2000);
+//   });
+// }
+
+// function getData() {
+//   foo().then(data => {
+//     data.map(el => {
+//       console.log(el);
+//     });
+//   });
+// }
+// logNumber();
