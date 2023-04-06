7819f9a143b92ac5998a2104b9eb8c02ecb6ceae
Andrii Valenko <andreyvalenko@gmail.com>
2023-04-06 15:29:53 +0300

    s

--- js/random.js
+++ js/random.js
@@ -1,26 +1,28 @@
-let a = 1;
+// let a = 1;
 
-let b = a;
+// let b = a;
 
-a = 2;
-console.log(b);
+// a = 2;
+// console.log(b);
 
-let objA = { name: 1 };
-let objB = objA;
+// let objA = { name: 1 };
+// let objB = objA;
 
-objB.name = 2;
+// objB.name = 2;
 
-console.log(objA);
+// console.log(objA);
 
-let foo = () => {
-  console.log(1);
-};
+// let foo = () => {
+//   console.log(1);
+// };
 
-let goo = foo;
+// let goo = foo;
 
-goo = () => {
-  console.log(2);
-};
+// goo = () => {
+//   console.log(2);
+// };
 
-foo();
-goo();
+// foo();
+// goo();
+
+const a = 'Hello';
