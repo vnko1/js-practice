ee668b7e40ee62c1b42933b511c1b9170e81e554
Andrii Valenko <andreyvalenko@gmail.com>
2023-04-14 16:50:16 +0300

    s

--- js/randomTest.js
+++ js/randomTest.js
@@ -1,1 +1,19 @@
 // console.log(a);
+
+var f = function () {
+  console.log(1);
+};
+
+var execute = function (f) {
+  setTimeout(f, 1000);
+};
+
+f = function () {
+  console.log(2);
+};
+
+// execute(f); // что выведет в консоль и почему
+
+console.log(d);
+
+d = 2;
